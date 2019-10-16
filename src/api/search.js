c699a7fc9c49431a6507aad3e24cbc482a14fcbd
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-10-16 21:24:35 +0800

    展示搜索结果
    展示搜索结果

--- src/api/search.js
+++ src/api/search.js
@@ -4,10 +4,24 @@
 
 import request from '@/utils/request'
 
+/**
+ * 获取联想建议（自动补全）
+ */
 export function searchSuggestion (params) {
   return request({
     url: '/app/v1_0/suggestion',
     method: 'get',
+    params
+  })
+}
+
+/**
+ * 获取搜索结果
+ */
+export function getSearch (params) {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/search',
     params
   })
 }
--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="serach-result">
     <!-- 导航栏 -->
-    <van-nav-bar title="xxx 的搜索结果" left-arrow fixed @click-left="$router.back()" />
+    <van-nav-bar :title="$route.params.q + '的搜索结果'" left-arrow fixed @click-left="$router.back()" />
     <!-- /导航栏 -->
 
     <!-- 文章列表 -->
@@ -12,38 +12,45 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell v-for="(item,index) in searchResultList" :key="index" :title="item.title" />
     </van-list>
     <!-- /文章列表 -->
   </div>
 </template>
 
 <script>
+import { getSearch } from '@/api/search'
+
 export default {
   name: 'SearchResult',
   data () {
     return {
-      list: [],
+      searchResultList: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      perPage: 10
     }
   },
 
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+    async onLoad () {
+      // 1.获取数据
+      const { data } = await getSearch({
+        page: this.page,
+        per_page: this.perPage,
+        q: this.$route.params.q
+      })
+      // 2.数据加载到数组中
+      this.searchResultList.push(...data.results)
+      // 3.加载状态结束
+      this.loading = false
+      // 4.判断是否还有数据
+      if (data.page) {
+        this.page++
+      } else {
+        this.finished = true
+      }
     }
   }
 }
