92cd77132bd595c741588cb37bd02263f065f102
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-10-19 19:31:43 +0800

    个人信息-头像上传预览
    个人信息-头像上传预览

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-    <van-nav-bar
-      title="个人信息"
-      left-arrow
-      right-text="保存"
-    />
+    <van-nav-bar title="个人信息" left-arrow right-text="保存" />
     <van-cell-group>
-      <van-cell title="头像" is-link>
-        <van-image
-          round
-          width="30"
-          height="30"
-          :src="user.photo"
-        />
+      <van-cell title="头像" is-link @click="onChangePhoto">
+        <van-image round width="30" height="30" :src="user.photo" />
       </van-cell>
       <van-cell title="昵称" :value="user.name" is-link />
       <van-cell title="性别" :value="user.gender ? '男': '女' " is-link />
       <van-cell title="生日" :value="user.birthday" is-link />
     </van-cell-group>
+
+    <!-- 上传头像文件 隐藏 -->
+    <input type="file" hidden ref="photoFile" @change="onPhotoFile" />
   </div>
 </template>
 
@@ -28,7 +22,7 @@ export default {
   name: 'UserIndex',
   data () {
     return {
-      user: {}
+      user: {} // 用户
     }
   },
 
@@ -37,10 +31,29 @@ export default {
   },
 
   methods: {
+    /**
+     * 加载用户
+     */
     async loadUser () {
       const { data } = await getProfile()
       this.user = data
+    },
+
+    /**
+     * 点击头像事件
+     */
+    onChangePhoto () {
+      this.$refs['photoFile'].click()
+    },
+    /**
+     * photoFile 改变事件
+     */
+    onPhotoFile () {
+      if (this.$refs['photoFile'].files.length) {
+        this.user.photo = window.URL.createObjectURL(this.$refs['photoFile'].files[0])
+      }
     }
+
   }
 }
 </script>
